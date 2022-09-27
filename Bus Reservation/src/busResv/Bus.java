c8d9d457f390ad66990338295c3751234bee4503
PrakashAK <prakashtazk@gmail.com>
2022-09-27 08:10:17 +0530

    Initial commit

---
+++ Bus Reservation/src/busResv/Bus.java
@@ -0,0 +1,23 @@
+package busResv;
+
+public class Bus {
+	
+	private int busNo;
+	private boolean ac;
+	private int capacity;
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public int getbusNo() {
+		return busNo;
+	}
+	public void setbusNo (int busNo) {
+		this.busNo = busNo;
+	}
+	
+
+
+}
